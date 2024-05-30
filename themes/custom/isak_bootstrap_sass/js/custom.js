337a7b4251e0c1b1e257398bcfc2e72d4c457f57
Sunil Pawar <sunil@skvare.com>
2024-05-30 07:22:01 -0400

    first commit

---
+++ themes/custom/isak_bootstrap_sass/js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.isak_bootstrap_sass = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
